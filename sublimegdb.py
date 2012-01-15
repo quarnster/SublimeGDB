96302c759d18853a5ab3136d0aec665e0cdc48a6
Fredrik Ehnbom <quarnster@gmail.com>
2012-01-15 14:49:17 +0100

    "commandline" setting. GDB stdout/stderr to a view. Basic breakpoint sync

--- sublimegdb.py
+++ sublimegdb.py
@@ -4,14 +4,38 @@ import subprocess
 import threading
 import time
 import traceback
+import sys
 
 breakpoints = {}
 
 
 def update(view):
-    #view.add_regions("sublimegdb.breakpoints", breakpoints[view.file_name()], "keyword", "circle", sublime.HIDDEN)
+    bps = []
+    for line in breakpoints[view.file_name()]:
+        bps.append(view.full_line(view.text_point(line - 1, 0)))
+    view.add_regions("sublimegdb.breakpoints", bps, "keyword.gdb", "circle", sublime.HIDDEN)
     #if hit_breakpoint:
-    view.add_regions("sublimegdb.position", breakpoints[view.file_name()], "entity.name.class", "bookmark", sublime.HIDDEN)
+
+    # cursor: view.add_regions("sublimegdb.position", breakpoints[view.file_name()], "entity.name.class", "bookmark", sublime.HIDDEN)
+
+def add_breakpoint(filename, line):
+    breakpoints[filename].append(line)
+
+def remove_breakpoint(filename, line):
+    breakpoints[filename].remove(line)
+
+def toggle_breakpoint(filename, line):
+    if line in breakpoints[filename]:
+        remove_breakpoint(filename, line)
+    else:
+        add_breakpoint(filename, line)
+
+def sync_breakpoints():
+    for file in breakpoints:
+        for bp in breakpoints[file]:
+            cmd = "break %s:%d\n" % (file, bp)
+            print cmd
+            gdb_process.stdin.write(cmd)
 
 
 class GdbToggleBreakpoint(sublime_plugin.TextCommand):
@@ -19,37 +43,71 @@ class GdbToggleBreakpoint(sublime_plugin.TextCommand):
         fn = self.view.file_name()
         if fn not in breakpoints:
             breakpoints[fn] = []
-        region = self.view.full_line(self.view.sel()[0])
-        if region in breakpoints[fn]:
-            breakpoints[fn].remove(region)
-        else:
-            breakpoints[fn].append(region)
+
+        line, col = self.view.rowcol(self.view.sel()[0].a)
+        toggle_breakpoint(fn, line + 1)
         update(self.view)
 
 gdb_process = None
+lock = threading.Lock()
+output = []
+
+def get_view():
+    gdb_view = sublime.active_window().open_file("GDB Session")
+    gdb_view.set_scratch(True)
+    gdb_view.set_read_only(True)
+    return gdb_view
+
+def update_view():
+    global output
+    lock.acquire()
+    try:
+        gdb_view = get_view()
+        if (gdb_view.is_loading()):
+            sublime.set_timeout(update_view, 100)
+            return
+
+        e = gdb_view.begin_edit()
+        try:
+            gdb_view.set_read_only(False)
+            for line in output:
+                gdb_view.insert(e, gdb_view.size(), line)
+            gdb_view.set_read_only(True)
+            gdb_view.show(gdb_view.size())
+            output = []
+        finally:
+            gdb_view.end_edit(e)
+    finally:
+        lock.release()
 
 
-def gdboutput():
+
+def gdboutput(pipe):
     global gdb_process
+    global old_stdin
+    global lock
+    global output
     while True:
         try:
             if gdb_process.poll() != None:
                 break
-            line = gdb_process.stdout.readline().strip()
+            line = pipe.readline().replace("(gdb)", "").strip()
             if len(line) > 0:
-                print "%s" % line
+                lock.acquire()
+                output.append("%s\n" % line)
+                lock.release()
+                sublime.set_timeout(update_view, 0)
         except:
             traceback.print_exc()
-
-def gdbkill():
-    global gdb_process
-    time.sleep(2)
-    gdb_process.stdin.write("quit\n")
-    gdb_process.wait()
+    if pipe == gdb_process.stdout:
+        lock.acquire()
+        output.append("GDB session ended\n")
+        lock.release()
+        sublime.set_timeout(update_view, 0)
 
 
 def show_input():
-    sublime.active_window().show_input_panel("GDB", "quit", input_on_done, input_on_change, input_on_cancel)
+    sublime.active_window().show_input_panel("GDB", "", input_on_done, input_on_change, input_on_cancel)
 
 class GdbInput(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -58,6 +116,8 @@ class GdbInput(sublime_plugin.TextCommand):
 
 def input_on_done(s):
     gdb_process.stdin.write("%s\n" % s)
+    if s.strip() != "quit":
+        show_input()
 
 def input_on_cancel():
     pass
@@ -65,13 +125,30 @@ def input_on_cancel():
 def input_on_change(s):
     pass
 
+
+def get_setting(key, default=None):
+    try:
+        s = sublime.active_window().active_view().settings()
+        if s.has("sublimegdb_%s" % key):
+            return s.get("sublimegdb_%s" % key)
+    except:
+        pass
+    return sublime.load_settings("SublimeGDB.sublime-settings").get(key, default)
+
+
+
 class GdbLaunch(sublime_plugin.TextCommand):
     def run(self, edit):
         global gdb_process
+        global gdb_view
         if gdb_process == None or gdb_process.poll() != None:
-            gdb_process = subprocess.Popen("gdb", shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            gdb_process = subprocess.Popen(get_setting("commandline"), shell=False, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            sync_breakpoints()
 
-            t = threading.Thread(target=gdboutput)
+            t = threading.Thread(target=gdboutput, args=(gdb_process.stdout,))
+            t.start()
+
+            t = threading.Thread(target=gdboutput, args=(gdb_process.stderr,))
             t.start()
 
             show_input()
