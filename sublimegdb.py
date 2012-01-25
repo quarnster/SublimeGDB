48f0e0e6e14f52c0be41a290378388db12e6f0ca
Fredrik Ehnbom <quarnster@gmail.com>
2012-01-25 20:26:28 +0100

    Removed the view index setting. It won't work anyway now that views can be disabled, closed and reopened dynamically.

--- sublimegdb.py
+++ sublimegdb.py
@@ -782,11 +782,7 @@ class GdbLaunch(sublime_plugin.WindowCommand):
                 if view.is_closed() and view.open_at_start():
                     view.open()
                 view.clear()
-            # setting the view index keeps crashing my Linux...
-            #w.set_view_index(gdb_session_view.get_view(), session_group, get_setting("session_index", 0))
-            #w.set_view_index(gdb_console_view.get_view(), console_group, get_setting("console_index", 1))
-            #w.set_view_index(gdb_variables_view.get_view(), variables_group, get_setting("variables_index", 2))
-            #w.set_view_index(gdb_callstack_view.get_view(), callstack_group, get_setting("callstack_index", 0))
+
             t = threading.Thread(target=gdboutput, args=(gdb_process.stdout,))
             t.start()
             try:
