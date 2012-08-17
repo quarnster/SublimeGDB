e2a69dcf584dc45d0e932d2625458ae5bcb3471c
Fredrik Ehnbom <quarnster@gmail.com>
2012-08-17 18:24:35 +0200

    Possible aid for issue #26

--- sublimegdb.py
+++ sublimegdb.py
@@ -71,7 +71,7 @@ def expand_path(value, window):
         ]
     value = re.sub(r'\${project_path:(?P<file>[^}]+)}', lambda m: len(get_existing_files(m)) > 0 and get_existing_files(m)[0] or m.group('file'), value)
     value = re.sub(r'\${env:(?P<variable>.*)}', lambda m: os.getenv(m.group('variable')), value)
-    value = re.sub(r'\${home}', os.getenv('HOME'), value)
+    value = re.sub(r'\${home}', re.escape(os.getenv('HOME')) if os.getenv("HOME") else "HOME_NOT_SET", value)
     value = re.sub(r'\${folder:(?P<file>.*)}', lambda m: os.path.dirname(m.group('file')), value)
     value = value.replace('\\', '/')
 
